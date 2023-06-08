aa788391396f5294b9e31d4a82b6b7bb3b13f613
Isabel González <isaabelglez@gmail.com>
2023-06-08 20:01:36 +0100

    add function calculadora with if

--- netlify/functions/calculadora/calculadora.js
+++ netlify/functions/calculadora/calculadora.js
@@ -1,17 +1,27 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    const num1 = parseInt(event.queryStringParameters.num1) || 0
-    const num2 = parseInt(event.queryStringParameters.num2) || 0
-
-    const sumar = num1 + num2
-    const restar = num1 - num2
-    const dividir = num1 / num2
-    const multiplicar = num1 * num2
+    function calculadora(operacion, num1, num2) {
+      if (operacion === 'suma') {
+        return num1 + num2;
+      } else if (operacion === 'resta') {
+        return num1 - num2;
+      } else if (operacion === 'multiplicacion') {
+        return num1 * num2;
+      } else if (operacion === 'division') {
+        if (num2 !== 0) {
+          return num1 / num2;
+        } else {
+          return 'No se puede dividir entre cero';
+        }
+      } else {
+        return 'Operación no válida';
+      }
+    }
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: sumar, restar, dividir, multiplicar }),
+      body: JSON.stringify({ message: calculadora}),
       
     }
   } catch (error) {
